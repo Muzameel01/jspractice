c4c74b6a90226f991bfa5b569f90eec3d1a5c0c9
Muzameel01 <mkmujameel@gmail.com>
2023-01-20 09:05:25 +0530

    day 10 practice

---
+++ stringmethod2.js
@@ -0,0 +1,7 @@
+
+let text="apple, banana, kiwi, banana";
+let index= text.indexOf("banana")
+console.log("banana position is::",index);
+const newstring=text.slice(index);
+console.log("the new string is::",newstring);
+
